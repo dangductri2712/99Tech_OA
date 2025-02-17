3f3847d99a2fefc08448b9fcab1d83ba3abc5f0f
dangductri2712 <dangductri2712@gmail.com>
2025-02-17 21:40:37 +0700

    Change the sum_to_n.ts file

--- Backend/dist/sum_to_n.js
+++ Backend/dist/sum_to_n.js
@@ -1,5 +1,4 @@
 "use strict";
-//assuming that "n" >=0
 //#1: use a for loop to sum up every iteration until reach the "n". Time complexity is O(n)
 function sum_to_n_a(n) {
     var result = 0;
@@ -8,7 +7,7 @@ function sum_to_n_a(n) {
     }
     return result;
 }
-//#2:  use a while loop. This also has time complexity of O(n)
+//#2:  use a while loop. Time complexity is O(n)
 function sum_to_n_b(n) {
     var result = 0;
     var currIndex = 1;
@@ -18,12 +17,12 @@ function sum_to_n_b(n) {
     }
     return result;
 }
-//#3: Following this format: result = (n-n+1)+(n-n+2)+ (n-n+3)+...+n using recursive. Time complexity is O(n)
+//#3: using recursion. Time complexity is O(n)
 function sum_to_n_c(n) {
-    if(n == 1){
+    if (n == 1) {
         return 1;
     }
-    return n+ sum_to_n_c(n-1);
+    return n + sum_to_n_c(n - 1);
 }
 console.log(sum_to_n_a(5));
 console.log(sum_to_n_b(5));
--- Backend/src/sum_to_n.ts
+++ Backend/src/sum_to_n.ts
@@ -1,4 +1,4 @@
-//#1: use a for loop to sum up every iteration until reach the "n"
+//#1: use a for loop to sum up every iteration until reach the "n". Time complexity is O(n)
 function sum_to_n_a(n: number): number{
     var result:number = 0;
     for(var i=1; i<= n; i++){
@@ -7,7 +7,7 @@ function sum_to_n_a(n: number): number{
     return result;
 }
 
-//#2:  use a while loop. Need to think later.
+//#2:  use a while loop. Time complexity is O(n)
 function sum_to_n_b(n: number): number{
     var result: number = 0;
     var currIndex = 1;
@@ -18,14 +18,12 @@ function sum_to_n_b(n: number): number{
     return result;
 }
 
-//#3: Following this format: result = (n-n+1)+(n-n+2)+ (n-n+3)+...+n
+//#3: using recursion. Time complexity is O(n)
 function sum_to_n_c(n: number): number{
-    var result: number = 0;
-    for(var i:number =n-1; i>=0; i--){
-        result+= (n-i);
+    if(n == 1){
+        return 1;
     }
-
-    return result;
+    return n+ sum_to_n_c(n-1);
 }
 console.log(sum_to_n_a(5));
 console.log(sum_to_n_b(5));
